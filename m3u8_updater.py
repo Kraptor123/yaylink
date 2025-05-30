193e8d80dff79b7a252c9db7d86677bb238f22db
Kraptor123 <89366989+Kraptor123@users.noreply.github.com>
2025-05-30 21:50:46 +0300

    Update m3u8_updater.py

--- m3u8_updater.py
+++ m3u8_updater.py
@@ -19,8 +19,8 @@ class M3U8Updater:
                 'referrer': 'https://taraftarium.co/'
             },
             {
-                'page_url': 'https://trgoals1336.xyz/channel.html?id=yayin1',
-                'referrer': 'https://trgoals1336.xyz/'
+                'page_url': 'https://trgoals1344.xyz/channel.html?id=yayin1',
+                'referrer': 'https://trgoals1344.xyz/'
             }
         ]
         self.baseurl_regex = re.compile(r'baseurl\s*=\s*["\'](https?://[^"\']+/)["\']')
